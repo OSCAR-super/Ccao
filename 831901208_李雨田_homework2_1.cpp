689fabf09deafa85971c14935459d7c4451f21c8
李雨田 <57757708+OSCAR-super@users.noreply.github.com>
2021-05-20 01:11:12 +0800

    2021.5.20

---
+++ 831901208_李雨田_homework2_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Amount,line;
+    Amount=5;
+    line=Amount*(Amount-1)/2;
+    cout << "we need :" <<  line
+         <<  endl;
+    return 0;
+}
---
+++ 831901208_李雨田_homework2_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double  S,L,d,c,I;
+    I=21.4;
+    L=700;
+    d=8;
+    c=6;
+    S=L*d*c/I;
+    cout << "The value of S is:"  <<  S
+         <<  endl;
+    return 0;
+}
